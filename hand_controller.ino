3849b0cc7ab55f6d3601fab5fcd79fb61899cf37
Traunin <traunin5@gmail.com>
2025-04-08 02:29:53 +0300

    feat: moving servo based on sensor input

--- hand_controller.ino
+++ hand_controller.ino
@@ -4,10 +4,12 @@
 #define MAX1 120
 #define MIN2 60
 #define MAX2 120
-#define LOG_DELAY 50
+#define LOG_DELAY 100
+#define IDLE_RESISTANCE 775
 
 Servo servo1;
 Servo servo2;
+Servo tactile;
 
 long last_log = 0;
 long current = 0;
@@ -15,6 +17,7 @@ long current = 0;
 void setup() {
   servo1.attach(3);
   servo2.attach(5);
+  tactile.attach(6);
   pinMode(A7, INPUT);
   Serial.begin(9600);
 }
@@ -40,6 +43,14 @@ void loop() {
     current = millis();
     if (current - last_log > LOG_DELAY) {
       last_log = millis();
-      Serial.println(analogRead(A7));
+      updateTactile(analogRead(A7));
     }
+}
+
+void updateTactile(int inp) {
+  Serial.println(inp);
+  float diff = ((float) abs(inp - IDLE_RESISTANCE)) / 50.0;
+  int angle = min(90, max(0, (int) (diff * 90)));
+//  Serial.println(angle);
+  tactile.write(angle);
 }
