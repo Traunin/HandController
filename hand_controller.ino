b8c2ef8b37ac0acd62e69d850d0a0dcbfcfe4d8b
Traunin <traunin5@gmail.com>
2025-04-13 19:17:11 +0300

    feat: 3 tactile feedback loops

--- hand_controller.ino
+++ hand_controller.ino
@@ -12,21 +12,33 @@
 Servo servo1;
 Servo servo2;
 Servo servo3;
-Servo tactile;
+Servo tactile1;
+Servo tactile2;
+Servo tactile3;
 
 long lastLog = 0;
 long current = 0;
 
-float baseline;
-int tactileStrength = 0;
+float baseline1;
+int tactileStrength1 = 0;
+float baseline2;
+int tactileStrength2 = 0;
+float baseline3;
+int tactileStrength3 = 0;
 
 void setup() {
   servo1.attach(3);
   servo2.attach(5);
-  tactile.attach(6);
+  tactile1.attach(6);
   servo3.attach(9);
+  tactile2.attach(10);
+  tactile3.attach(11);
   pinMode(A7, INPUT);
-  baseline = analogRead(A7);
+  pinMode(A6, INPUT);
+  pinMode(A5, INPUT);
+  baseline1 = analogRead(A7);
+  baseline2 = analogRead(A6);
+  baseline3 = analogRead(A5);
   Serial.begin(9600);
 }
 
@@ -50,7 +62,7 @@ void loop() {
 
         if (number3 >= MIN3 && number3 <= MAX3) {
 //          Serial.print("Servo 3: ");
-//          Serial.println(number2);
+//          Serial.println(number3);
           servo3.write(number3);
         }
     }
@@ -58,18 +70,20 @@ void loop() {
     current = millis();
     if (current - lastLog > LOG_DELAY) {
       lastLog = current;
-      updateTactile(analogRead(A7));
+      updateTactile(analogRead(A7), tactileStrength1, baseline1, tactile1);
+      updateTactile(analogRead(A6), tactileStrength2, baseline2, tactile2);
+      updateTactile(analogRead(A5), tactileStrength3, baseline3, tactile3);
     }
 }
 
-void updateTactile(int inp) {
-  Serial.print(inp); Serial.print(" ");
+int updateTactile(int inp, int &currentTactile, float &baseline, Servo &tactile) {
+//  Serial.print(inp); Serial.print(" ");
   float diff = inp - baseline;
   baseline += diff * K;
   if (abs(diff) > 5) {
-      tactileStrength = min(max(tactileStrength - (int) (diff / 2), 0), 90);
-      Serial.print(tactileStrength);
-      tactile.write(tactileStrength);
+      currentTactile = min(max(currentTactile - (int) (diff / 2), 0), 90);
+//      Serial.print(tactileStrength);
+      tactile.write(currentTactile);
   }
-  Serial.println();
+//  Serial.println();
 }
