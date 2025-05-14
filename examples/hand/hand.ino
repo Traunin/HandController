2a15e0fb11ae4ed08738c4abbe40a1a0b644a518
Traunin <traunin5@gmail.com>
2025-05-14 12:35:09 +0300

    feat: use Hand class

---
+++ examples/hand/hand.ino
@@ -0,0 +1,29 @@
+#include <Hand.h>
+
+Finger finger1(3, 60, 120);
+Finger finger2(5, 60, 120);
+Finger finger3(9, 90, 160);
+Hand hand(3);
+
+
+void setup() {
+  finger1.attachBend(A7);
+  finger2.attachBend(A6);
+  finger3.attachBend(A5);
+  finger1.attachForce(A4);
+  finger1.attachForce(A3);
+  finger1.attachForce(A2);
+  
+  hand.setFinger(0, &finger1);
+  hand.setFinger(1, &finger2);
+  hand.setFinger(2, &finger3);
+  
+  Serial.begin(115200);
+}
+
+void loop() {
+  hand.tick();
+  hand.setFromSerial();
+
+  hand.logForce();
+}
