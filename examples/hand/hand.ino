ca59237f62d6cd7283741db027aded744027d59d
Traunin <traunin5@gmail.com>
2025-05-16 08:17:01 +0300

    feat: comments

--- examples/hand/hand.ino
+++ examples/hand/hand.ino
@@ -1,19 +1,23 @@
 #include <Hand.h>
 
+// pin, open angle, closed angle
 Finger finger1(3, 60, 120);
 Finger finger2(5, 60, 120);
 Finger finger3(9, 90, 160);
-Hand hand(3);
+Hand hand(3); // initialize a hand with 3 fingers
 
 
 void setup() {
+  // attaching bend sensors
   finger1.attachBend(A7);
   finger2.attachBend(A6);
   finger3.attachBend(A5);
+  // attaching force sensors
   finger1.attachForce(A4);
   finger2.attachForce(A3);
   finger3.attachForce(A2);
   
+  // joining fingers into a hand
   hand.setFinger(0, &finger1);
   hand.setFinger(1, &finger2);
   hand.setFinger(2, &finger3);
@@ -22,8 +26,11 @@ void setup() {
 }
 
 void loop() {
+  // every loop, call tick to update sensors
   hand.tick();
+  // if there is data in serial, bend fingers
+  // for 3 fingers: [0-255] [0-255] [0-255]
   hand.setFromSerial();
-
+  // print tactile forces to serial
   hand.logForce();
 }
