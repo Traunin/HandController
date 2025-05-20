823d4e4ff4e87f6f5eb6afd947632854693cd9df
Traunin <traunin5@gmail.com>
2025-05-20 17:44:10 +0300

    fix: initialize fingers in setup

--- examples/hand/hand.ino
+++ examples/hand/hand.ino
@@ -1,12 +1,14 @@
 #include <Hand.h>
 
-Finger finger1(3, 60, 120);
-Finger finger2(5, 60, 120);
-Finger finger3(9, 90, 160);
+Finger* finger1;
+Finger* finger2;
+Finger* finger3;
 Hand hand(3);
 
-
 void setup() {
+  finger1 = new Finger(3, 60, 120);
+  finger2 = new Finger(5, 60, 120);
+  finger3 = new Finger(9, 90, 160);
   finger1.attachBend(A7);
   finger2.attachBend(A6);
   finger3.attachBend(A5);
@@ -14,9 +16,9 @@ void setup() {
   finger2.attachForce(A3);
   finger3.attachForce(A2);
   
-  hand.setFinger(0, &finger1);
-  hand.setFinger(1, &finger2);
-  hand.setFinger(2, &finger3);
+  hand.setFinger(0, finger1);
+  hand.setFinger(1, finger2);
+  hand.setFinger(2, finger3);
   
   Serial.begin(115200);
 }
