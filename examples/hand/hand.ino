610f03b45dd2da36923ae15a0cc808833f13e72e
Traunin <traunin5@gmail.com>
2025-05-20 18:48:54 +0300

    fix: pointer method call

--- examples/hand/hand.ino
+++ examples/hand/hand.ino
@@ -11,13 +11,13 @@ void setup() {
   finger1 = new Finger(3, 60, 120);
   finger2 = new Finger(5, 60, 120);
   finger3 = new Finger(9, 90, 160);
-  finger1.attachBend(A7);
-  finger2.attachBend(A6);
-  finger3.attachBend(A5);
+  finger1 -> attachBend(A7);
+  finger2 -> attachBend(A6);
+  finger3 -> attachBend(A5);
   // attaching force sensors
-  finger1.attachForce(A4);
-  finger2.attachForce(A3);
-  finger3.attachForce(A2);
+  finger1 -> attachForce(A4);
+  finger2 -> attachForce(A3);
+  finger3 -> attachForce(A2);
   
   // joining fingers into a hand
   hand.setFinger(0, finger1);
