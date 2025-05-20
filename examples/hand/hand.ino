881716e35f429d2e641ad646ffd271b4a0534ec6
Traunin <traunin5@gmail.com>
2025-05-21 02:36:25 +0300

    style: empty lines, -> spacing

--- examples/hand/hand.ino
+++ examples/hand/hand.ino
@@ -1,23 +1,25 @@
 #include <HandController.h>
 
-// pin, open angle, closed angle
 Finger* finger1;
 Finger* finger2;
 Finger* finger3;
 Hand hand(3); // initialize a hand with 3 fingers
 
 void setup() {
-  // attaching bend sensors
+  // pin, open angle, closed angle
   finger1 = new Finger(3, 60, 120);
   finger2 = new Finger(5, 60, 120);
   finger3 = new Finger(9, 90, 160);
-  finger1 -> attachBend(A7);
-  finger2 -> attachBend(A6);
-  finger3 -> attachBend(A5);
+
+  // attaching bend sensors
+  finger1->attachBend(A7);
+  finger2->attachBend(A6);
+  finger3->attachBend(A5);
+
   // attaching force sensors
-  finger1 -> attachForce(A4);
-  finger2 -> attachForce(A3);
-  finger3 -> attachForce(A2);
+  finger1->attachForce(A4);
+  finger2->attachForce(A3);
+  finger3->attachForce(A2);
   
   // joining fingers into a hand
   hand.setFinger(0, finger1);
